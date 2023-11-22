e02a2875c9b4f055b646711ffa4da181579daa6f
Tyshe-81 <adeniyioluwatishe@gmail.com>
2023-11-22 08:12:37 -0800

    Initial Commit

---
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,13 @@
+package src.main.java.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringbootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+
+}
